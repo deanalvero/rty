8c31d1c433d4fbc54b08a2966f3f3aba4acde8b7
Dean Alvero <deanalvero@gmail.com>
2015-05-15 11:51:20 +0800

    initial commit

--- rty/ViewController.m
+++ rty/ViewController.m
@@ -12,16 +12,38 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    NSArray *data;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    data = [NSArray arrayWithObjects:@"Cupcake", @"Donut", @"Eclair", @"Froyo", @"Gingerbread", @"Honeycomb", @"Ice Cream Sandwich", @"Jelly Bean", @"KitKat", @"Lollipop", nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [data count];
+}
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *tableIdentifierAndroid = @"AndroidTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: tableIdentifierAndroid];
+    
+    if (cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableIdentifierAndroid];
+    }
+    
+    cell.textLabel.text = [data objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"beastmaster.png"];
+    return cell;
 }
 
 @end
